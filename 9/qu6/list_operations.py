541e4387d054988f99d767e051abf8b73577f7af
TitledApollo <mantritbar@gmail.com>
2025-12-12 02:45:34 +0530

    Delete 9 directory

--- 9/qu6/list_operations.py
+++
@@ -1,8 +0,0 @@
-# list_operations.py
-
-# Function to find the largest and smallest number in a list
-def find_largest_smallest(lst):
-    largest = max(lst)
-    smallest = min(lst)
-    return largest, smallest
-

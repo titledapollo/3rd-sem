27ce34b55416b07609b2c666b5dc3b6e3d7104e6
TitledApollo <mantritbar@gmail.com>
2025-12-12 02:44:55 +0530

    Delete 5 directory

--- 5/rep.py
+++
@@ -1,9 +0,0 @@
-n = tuple(input("Enter elements:\n").split())
-
-key = input("Which element to find? : ")
-found = 0
-for i in range (len(n)):
-	if key in n[i]:
-		found = found+1
-print("The element repeated is: ",key)
-print("Repeated times: ",found)
--- 5/rev.py
+++
@@ -1,5 +0,0 @@
-n = tuple(input("Enter elements: ").split())
-
-rev = n[::-1]
-
-print("Reversed tuple:", rev)

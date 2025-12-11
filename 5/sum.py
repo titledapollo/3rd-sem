8397d881e1920970201c154a1fd3389df6df0922
TitledApollo <mantritbar@gmail.com>
2025-12-12 02:37:09 +0530

    Delete 5 directory

--- 5/sum.py
+++
@@ -1,7 +0,0 @@
-n = tuple(map(int, input("Enter numbers:\n").split()))
-
-total = 0
-for i in n:
-    total += i
-
-print("Sum of all numbers:", total)
--- 5/tup.py
+++
@@ -1,5 +0,0 @@
-n = tuple(map(int, input("Enter numbers:\n").split()))
-
-tup = tuple(sorted(n))
-
-print("Sorted tuple:", tup)

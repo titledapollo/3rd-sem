78d32fcfeb64c5c958ecc3a231dbc1a0a9f64ce6
TitledApollo <mantritbar@gmail.com>
2025-12-12 02:36:51 +0530

    Delete 3 directory

--- 3/len.py
+++
@@ -1,5 +0,0 @@
-text = input("Enter a Sentence: ")
-count = 0
-for char in text:
-    count += 1
-print("Length of the Sentence is: ",count)
--- 3/palid.py
+++
@@ -1,8 +0,0 @@
-input_string = input("Enter a string: ")
-
-processed_string = input_string.replace(" ", "").lower()
-
-if processed_string == processed_string[::-1]:
-    print("The string is a palindrome.")
-else:
-    print("The string is not a palindrome.")

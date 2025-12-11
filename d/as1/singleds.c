0a2d21b46159a3944533afdeebab39da7a6fc591
TitledApollo <mantritbar@gmail.com>
2025-12-12 02:49:17 +0530

    Delete d directory

--- d/as1/singleds.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-  printf("size of int : %lu bytes\n",sizeof(int));
-  printf("size of float : %lu bytes\n",sizeof(float));
-  printf("size of double : %lu bytes\n",sizeof(double));
-  printf("size of char : %lu bytes\n",sizeof(char));
-  return 0;
-  }

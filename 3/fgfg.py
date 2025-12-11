5bf48575586047ac8744075910a5b47c1cb28121
TitledApollo <mantritbar@gmail.com>
2025-12-12 02:44:36 +0530

    Delete 3 directory

--- 3/fgfg.py
+++
@@ -1,9 +0,0 @@
-input_string = input("Enter a string: ")
-prefix_length = int(input("Enter the length of the prefix to add: "))
-
-if prefix_length <= len(input_string):
-    prefix = input_string[:prefix_length]
-    result = prefix + input_string
-    print("Modified String:", result)
-else:
-    print("Error: Prefix length is greater than the length of the string.")
--- 3/reverse.py
+++
@@ -1,3 +0,0 @@
-input_string = input("Enter a string: ")
-reversed_string = input_string[::-1]
-print("Reversed String:", reversed_string)
